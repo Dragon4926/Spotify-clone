cfb429f8543c9b8d56071dfe147f386b66c35b3f
Debopriyo Das <70844598+Dragon4926@users.noreply.github.com>
2024-09-26 20:00:44 +0530

    upgraded server to ES6+

--- js/server.js
+++ js/server.js
@@ -1,5 +1,9 @@
-const express = require('express')
+import express from express
 const app = express()
 const port = 3000
 
 app.use(express.static(__dirname))
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
